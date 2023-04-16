909959be3f37404ac4f641c512c9a3c9f80c03c1
MimoudiX <mohcinemimoudi@gmail.com>
2023-04-16 03:22:58 +0000

    deleted .Next

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'localhost'
-const port = 3000
+const hostname =
+  process.env.NODE_ENV !== 'production' ? 'localhost' : 'mimoudix.com'
+const port = process.env.PORT || 4789
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
