498a8e2a051be6cc4abe3f36421b98ad38a05d05
MimoudiX <mohcinemimoudi@gmail.com>
2023-04-16 03:36:09 +0000

    first edit test

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -250,9 +250,7 @@ export default function Home({ articles }) {
   return (
     <>
       <Head>
-        <title>
-          Spencer Sharp - Software designer, founder, and amateur astronaut
-        </title>
+        <title>MimoudiX - Software Engineer</title>
         <meta
           name="description"
           content="I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
